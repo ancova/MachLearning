7c07101f189d2468782e1daf6091d0c9407a85ae
Canqing YU <yucanqing@outlook.com>
2014-07-27 21:22:30 +0800

    Final documents

---
+++ Submission.R
@@ -0,0 +1,11 @@
+answers <- as.character(predict(modFit, testing))
+answers
+pml_write_files = function(x){
+        n = length(x)
+        for(i in 1:n){
+                filename = paste0("problem_id_",i,".txt")
+                write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        }
+}
+
+pml_write_files(answers)
